3b2996ebc44f358eaf3e36d0737b132396f238e2
Param-Veer-Singh <122340319+Param-Veer-Singh@users.noreply.github.com>
2023-10-19 17:28:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let n = arr.length;
+	let ans = infinity;
+	for(let i=0; i<n-2; i++){
+		for(let j=i+1; j<n-1; j++){
+			for(let k=j+1; k<n; k++){
+				let sum = arr[i]+arr[j]+arr[k];
+				if(Math.abs(target - sum) < ans){
+					ans = sum;
+				}
+			}
+		}
+	}
+	return ans;
 }
 
 module.exports = threeSum;
